2e73d24d89abba283f8354e8cc765e96f2d5a3da
SkandaAithal <skandaaithal1.com>
2023-09-04 15:38:57 +0530

    Javascript click functionality added to Hamburger

--- sunnyside-agency-landing-page-main/App.js
+++ sunnyside-agency-landing-page-main/App.js
@@ -1,10 +1,14 @@
+let x = 0;
 document.querySelector(".hamburger").addEventListener("click", function () {
-  document.querySelector(".dropdown").style.visibility = "visible";
-  document.querySelector(" .body-layout main h1").style.display = "none";
-  document.querySelector(" .body-layout main svg").style.display = "none";
-});
-document.querySelector("main").addEventListener("click", function () {
-  document.querySelector(".dropdown").style.visibility = "hidden";
-  document.querySelector(" .body-layout main h1").style.display = "block";
-  document.querySelector(" .body-layout main svg").style.display = "block";
+  if (x === 0) {
+    document.querySelector(".dropdown").style.visibility = "visible";
+    document.querySelector(" .body-layout main h1").style.display = "none";
+    document.querySelector(" .body-layout main svg").style.display = "none";
+    x = 1;
+  } else {
+    document.querySelector(".dropdown").style.visibility = "hidden";
+    document.querySelector(" .body-layout main h1").style.display = "block";
+    document.querySelector(" .body-layout main svg").style.display = "block";
+    x = 0;
+  }
 });
